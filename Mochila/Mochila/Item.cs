6112bbeb5ab2aa18bf61217e2208ead40586de91
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-06-11 11:05:44 -0300

    Criando exemplo de AG com caso da Mochila

---
+++ Mochila/Mochila/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mochila
+{
+    public class Item
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Valor { get; set; }
+
+        public Item(long id, string nome, double peso, double valor)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Peso = peso;
+            this.Valor = valor;
+        }
+    }
+}
---
+++ Mochila/Mochila/Mochila.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mochila
+{
+    public class Mochila
+    {
+        public long Id { get; set; }
+        public double Peso { get; set; }
+        public double Valor { get; set; }
+        public IList<Item> Itens { get; set; }
+        public double PesoMaximo { get; set; }
+
+        public Mochila(double pesoMaximo)
+        {
+            this.PesoMaximo = pesoMaximo;
+        }
+
+        public void AdicionaItem(Item item)
+        {
+            if (verificaPesoDoItem(item) && verificaPesoMaximoDaMochila())
+            {
+                this.Itens.Add(item);
+                this.Peso += item.Peso;
+            }
+        }
+
+        private bool verificaPesoDoItem(Item item){
+            if (item.Peso < this.PesoMaximo)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool verificaPesoMaximoDaMochila()
+        {
+            if (this.Peso < this.PesoMaximo)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
